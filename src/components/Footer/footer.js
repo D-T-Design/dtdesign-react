2ab4a002b3e4b6574f0d574f690724d2285bdd4f
David Torres <info@davidtorres.design>
2018-12-06 07:09:05 -0800

    Add Github link to Footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -7,6 +7,7 @@ const Footer = () => (
       <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <div className="footer-div">
           &copy; 2016 - {new Date().getFullYear()} David Torres Design
+          <p>This website is also a React app!  Find out more by visiting my <a href="https://github.com/D-T-Design/dtdesign-react"><ion-icon name="logo-github" /> Github page</a>.</p>
         </div>
       </div>
     </div>
